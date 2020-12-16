b79ac9883853be44b830e06aaf11b1a2822098c5
okdyy75 <okdyy75@gmail.com>
2020-12-16 17:33:26 +0900

    configをexample化

---
+++ config/example.env.js
@@ -0,0 +1,12 @@
+'use strict'
+module.exports = {
+  FIREBASE_CONFIG : {
+    apiKey: '"<apiKey>"',
+    authDomain: '"<projectId>.firebaseapp.com"',
+    databaseURL: '"https://<projectId>.firebaseio.com"',
+    projectId: '"<projectId>"',
+    storageBucket: '"<projectId>.appspot.com"',
+    messagingSenderId: '"<messagingSenderId>"',
+    appId: "'<appId>'"
+  }
+}
